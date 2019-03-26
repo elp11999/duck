297bf22df86ab77f63986a6b47420092dc89bd70
Michael Henderson <mhenderson557@yahoo.com>
2019-03-26 02:32:20 -0500

    Updates

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,7 +5,8 @@ var path = require("path");
 module.exports = function(app) {
   // Load Splash page
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname + "/splash.html"));
+    console.log("__dirname=" + __dirname);
+    res.sendFile(path.join(__dirname + "/../public/splash.html"));
     /*
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
@@ -18,7 +19,20 @@ module.exports = function(app) {
 
   // Load Login page
   app.get("/login", function(req, res) {
-    res.sendFile(path.join(__dirname + "/login.html"));
+    res.sendFile(path.join(__dirname + "/../public/login.html"));
+    /*
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("index", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+    */
+  });
+
+  // Load Main page
+  app.get("/main", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/main.html"));
     /*
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
@@ -31,7 +45,9 @@ module.exports = function(app) {
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.render("example", {
         example: dbExample
       });
